e2c757d8efc4754eae0452530e6fbf553fba7019
PedroGtz16 <37647681+PedroGtz16@users.noreply.github.com>
2019-09-09 16:10:50 -0500

    Add files via upload

--- src/main/java/com/example/itesogram/itesogram.kt
+++ src/main/java/com/example/itesogram/itesogram.kt
@@ -6,20 +6,18 @@ import com.parse.Parse
 
 class itesogram (): Application(){
 
-    override fun onCreate() {
+    override fun onCreate(){
         super.onCreate()
         Parse.initialize(
-            Parse.Configuration.Builder(this).applicationId()
-            .clientKey()
-            .server()
-            .build()
-
-
-
-
+            Parse.Configuration.Builder(this)
+                .applicationId("v1kx8QckHwFoOhVF6JqOIkY77Y3ancPhksQnt4k6")
+                .clientKey("fVER38fcRHHMHtrS10xq8BnTAsQdrDKvoXSR3maP")
+                .server("https://parseapi.back4app.com/")
+                .build()
         )
     }
-
-
-
 }
+
+
+
+
--- src/main/java/com/example/itesogram/savingParse.kt
+++ src/main/java/com/example/itesogram/savingParse.kt
@@ -1,16 +1,21 @@
 package com.example.itesogram
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.picker.MaterialTextInputPicker
 import com.google.android.material.textfield.TextInputEditText
 import com.parse.ParseObject
+import com.parse.ParseQuery
+import kotlinx.android.synthetic.main.activity_savingparse.*
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 
 class savingParse : AppCompatActivity() {
 
+    private lateinit var textToGet : TextView
     private lateinit var textToSend: TextInputEditText
     private lateinit var sendParse: MaterialButton
     private lateinit var getParse: MaterialButton
@@ -21,14 +26,23 @@ class savingParse : AppCompatActivity() {
         sendParse = find(R.id.buttonSendParse)
         getParse = find(R.id.buttonGetParse)
         textToSend=find(R.id.parseSend)
+        textToGet = find(R.id.mensajeParse)
 
     //GET PARSE
         getParse.setOnClickListener {
 
-        
+            doAsync {
+                val query = ParseQuery<ParseObject>("Tarea")
+                query.whereEqualTo("expedientes", "is703761")
+                query.addDescendingOrder("createdAt")
+                query.getFirstInBackground{obj, e->
+                    if(e==null)
+                        mensajeParse.text = obj["valor"] as String
+                    else
+                        error{e}
 
-
-
+                }
+            }
 
         }
         //SEND PARSE
